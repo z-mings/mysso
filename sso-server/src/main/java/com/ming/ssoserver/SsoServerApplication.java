1d96ee7d46a867370f2c4b9917f4ced01af4c142
ming <15937685579@163.com>
2022-02-04 15:02:37 +0800

    创建项目

---
+++ sso-server/src/main/java/com/ming/ssoserver/SsoServerApplication.java
@@ -0,0 +1,16 @@
+package com.ming.ssoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ming
+ */
+@SpringBootApplication
+public class SsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApplication.class, args);
+    }
+
+}
